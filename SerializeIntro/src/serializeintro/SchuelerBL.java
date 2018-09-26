a9ab2e0098271b9f04118a128b03090d5c0cc059
Chris Schennach <senchb16@new.edvhtl.at>
2018-09-26 12:48:32 +0200

    Method save made

--- SerializeIntro/src/serializeintro/SchuelerBL.java
+++ SerializeIntro/src/serializeintro/SchuelerBL.java
@@ -5,10 +5,40 @@
  */
 package serializeintro;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import javax.swing.AbstractListModel;
+
 /**
  *
  * @author Christoph
  */
-public class SchuelerBL {
+public class SchuelerBL extends AbstractListModel {
+    private ArrayList<Schueler> klasse  = new ArrayList<Schueler>();
+
+    @Override
+    public int getSize() {
+        return klasse.size();
+    }
+
+    @Override
+    public Object getElementAt(int i) {
+        return klasse.get(i);
+    }
+    public void add(Schueler s){
+        klasse.add(s);
+    }
+    
+    public void save(File f)throws Exception{
+        BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+        for (Schueler s : klasse) {
+            bw.write(s.getAge());
+            bw.write(""+s.getAge());
+            bw.write(s.getBirthday().toString());
+            bw.newLine();
+        }
+    }
     
 }
